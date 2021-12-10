f669733010cadb90adce3f8a8ebd47fecdff5afb
Rezende123 <feliperezende.srz@gmail.com>
2021-12-10 19:42:49 -0300

    chore: Cria um controller para cuidar dos endpoints do user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { SiteAccessController } from './site-access/site-access.controller';
 import { CountapiService } from './site-access/services/countapi/countapi.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, SiteAccessController],
+  controllers: [AppController, SiteAccessController, UserController],
   providers: [AppService, CountapiService],
 })
 export class AppModule {}
