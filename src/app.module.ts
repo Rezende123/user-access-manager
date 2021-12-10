52193f01c7453fe5afeb289d96a955920277aa6b
Rezende123 <feliperezende.srz@gmail.com>
2021-12-10 19:18:51 -0300

    chore: Define variáveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SiteAccessController } from './site-access/site-access.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, SiteAccessController],
   providers: [AppService],
 })
