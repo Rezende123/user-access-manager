3b76c4ed8ce7969b1e567622f2f448a35c55406d
Rezende123 <feliperezende.srz@gmail.com>
2021-12-10 21:22:29 -0300

    feat: Criptografando a senha salva

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -3,14 +3,23 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserReturnAdapter } from '../adapters/user-return.adapter';
 import { User, UserDocument, UserInterface } from '../models/user.model';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
+  private async encriptPassword(userData: UserInterface) {
+    const rounds = Number(process.env.HASH_ROUNDS);
+    userData.password = await bcrypt.hash(userData.password, rounds);
+  }
+
   async create(userData: UserInterface) {
+    await this.encriptPassword(userData);
+
     const createdUser = new this.userModel(userData);
     const user = await createdUser.save();
+
     return new UserReturnAdapter(user);
   }
 }
