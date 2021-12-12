694dc53e4929f00b6fd800883cfa4c3e33eed7c8
Rezende123 <feliperezende.srz@gmail.com>
2021-12-12 06:55:58 -0300

    chore: Organiza melhor os arquivos em pastas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
 import { SiteAccessController } from './site-access/site-access.controller';
 import { CountapiService } from './site-access/services/countapi/countapi.service';
 import { ConfigModule } from '@nestjs/config';
